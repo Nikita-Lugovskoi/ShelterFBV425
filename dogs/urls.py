942f6afe23872b6f710bf7255ac49583773c1120
Nikita-Lugovskoi <maloooy11@gmail.com>
2025-05-02 12:45:11 +1000

    1. написаны breeds_list_view и breed_dogs_list_view. 2.написаны urls для breeds_list_view и breed_dogs_list_view. 3. добавлены пустые шаблоны breeds.html и dogs.html

--- dogs/urls.py
+++ dogs/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from dogs.apps import DogsConfig
 
-from dogs.views import index
+from dogs.views import index, breeds_list_view, breed_dogs_list_view
 
 app_name = DogsConfig.name
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('breeds/', breeds_list_view, name = 'breeds'),
+    path('breeds/<int:pk>/dogs/', breed_dogs_list_view, name='breed_dogs')
 ]
