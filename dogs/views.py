bb79774527c33a9aed1d2bb3d90cbb5fa058c6ef
Nikita-Lugovskoi <maloooy11@gmail.com>
2025-05-02 13:22:02 +1000

    1. написаны dogs_list_view и dogs_create_view. 2. написаны urls для dogs_list_view и dogs_create_view. 3. добавлен пустой шаблон create.html

--- dogs/views.py
+++ dogs/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from dogs.models import Breed, Dog
+from dogs.forms import DogForm
 
 
 def index(request):
     context = {
         'objects_list': Breed.objects.all()[:3],
-        'title': 'Питомник - Главное'
+        'title': 'Питомник - Главное',
     }
     return render(request, 'dogs/index.html', context=context)
 
@@ -14,7 +17,7 @@ def index(request):
 def breeds_list_view(request):
     context = {
         'objects_list': Breed.objects.all(),
-        'title': 'Питомник - Все наши породы'
+        'title': 'Питомник - Все наши породы',
     }
     return render(request, 'dogs/breeds.html', context=context)
 
@@ -25,6 +28,27 @@ def breed_dogs_list_view(request, pk: int):
     context = {
         'objects_list': Dog.objects.filter(breed_id=pk),
         'title': f'Собаки породы - {breed_object.name}',
-        'breed_pk': breed_object.pk
+        'breed_pk': breed_object.pk,
     }
     return render(request, 'dogs/dogs.html', context=context)
+
+
+def dogs_list_view(request):
+    context = {
+        'objects_list': Dog.objects.all,
+        'title': f'Все наши собаки',
+    }
+    return render(request, 'dogs/dogs.html', context)
+
+# Create Read Update Delet (CRUD)
+def dog_create_view(request):
+    if request.method == 'POST':
+        form = DogForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('dogs:dogs_list'))
+    context = {
+        'title': "Добавить собаку",
+        'form': DogForm,
+    }
+    return render(request, 'dogs/create.html', context=context)
